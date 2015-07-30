8535f163ad3ce346411de62a596fdcf108f60697
Denis Ciccale <dciccale@gmail.com>
2015-07-31 00:36:21 +0200

    tests fix #13

---
+++ test/css2stylus.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var assert = require('assert');
+
+var Css2Stylus = require('../lib/css2stylus');
+
+exports['Should convert CSS into Stylus'] = function (test) {
+ var css = 'body { color: red; }';
+  var converter = new Css2Stylus.Converter(css);
+  converter.processCss();
+  var stylusOutput = converter.getStylus();
+  console.log(stylusOutput);
+  test.equal(stylusOutput, 'body\n  color red\n\n');
+  test.done();
+};
