2fbc9feec6e7efc0e59d62e0ab6bdc11afec68a3
Kyle King <kmking72@gmail.com>
2015-11-29 10:18:22 -0500

    Created additional test cases

--- test/css2stylus.test.js
+++ test/css2stylus.test.js
@@ -14,12 +14,35 @@ exports['Should convert CSS into Stylus'] = function (test) {
   test.done();
 };
 
+exports['Should handle :hover'] = function (test) {
+  var css = fs.readFileSync('./test/fixture/hover.css').toString();
+  var styl = fs.readFileSync('./test/expected/hover.styl').toString();
+  var converter = new Css2Stylus.Converter(css);
+  converter.processCss();
+  var output = converter.getStylus();
+  // console.log(output);
+  test.equal(output, styl);
+  test.done();
+};
+
 exports['Should handle multiple @font-face declarations'] = function (test) {
   var css = fs.readFileSync('./test/fixture/font-face.css').toString();
   var styl = fs.readFileSync('./test/expected/font-face.styl').toString();
   var converter = new Css2Stylus.Converter(css);
   converter.processCss();
   var output = converter.getStylus();
+  // console.log(output);
+  test.equal(output, styl);
+  test.done();
+};
+
+exports['Should handle @media declarations'] = function (test) {
+  var css = fs.readFileSync('./test/fixture/media.css').toString();
+  var styl = fs.readFileSync('./test/expected/media.styl').toString();
+  var converter = new Css2Stylus.Converter(css);
+  converter.processCss();
+  var output = converter.getStylus();
+  console.log(output);
   test.equal(output, styl);
   test.done();
 };
